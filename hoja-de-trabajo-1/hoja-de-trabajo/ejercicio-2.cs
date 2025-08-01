c95cf5a1fbeea71a0d5462572e101aa4532ed082
Diego Gómez <DG6488379@gmail.com>
2025-07-31 23:24:37 -0600

    :zap:  se suben los primeros ejercicios de la hoja de trabajo #1

---
+++ hoja-de-trabajo-1/hoja-de-trabajo/ejercicio-2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoja_de_trabajo_1.hoja_de_trabajo
+{
+    internal class Usuario
+    {
+        public string Nombre;
+
+        private string dpi;
+
+        private string contraseña;
+
+        public string DPI
+        {
+            if
+        }
+        public void AsignarContraseña(string nuevaContraseña)
+        {
+            if (nuevaContraseña != null && nuevaContraseña.Length >= 6)
+            {
+                contraseña = nuevaContraseña;
+            }
+            else
+            {
+                Console.WriteLine("contraseña debe tener al menos 6 caracteres.");
+            }
+        }
+            public bool Autenticar(string input)
+        {
+            return contraseña == input;
+        }
+    }
+}
